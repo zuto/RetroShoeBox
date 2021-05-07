1725e0946a265619298fedd343712ffc7098699f
Jade Kneen <jade.kneen@zuto.com>
2021-05-07 11:51:13 +0100

    Made a start on the homemade authentication

---
+++ RetroShoeBox/Services/AuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetroShoeBox.Services
+{
+    public class AuthenticationService
+    {
+        public bool IsAuthenticated(string username, string password)
+        {
+            if (username == "Jade")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
